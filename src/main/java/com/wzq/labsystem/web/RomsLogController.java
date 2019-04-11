e65cb456ccc1e2799d1ddc0de247235988d6226c
wengzhenquan <wengzhenquan@duikavip.com>
2019-04-11 12:43:22 +0800

    修改BUG111

--- src/main/java/com/wzq/labsystem/web/RomsLogController.java
+++ src/main/java/com/wzq/labsystem/web/RomsLogController.java
@@ -10,10 +10,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(tags ={"3-2 实验室申请"})
@@ -24,7 +21,7 @@ public class RomsLogController {
 
 
     @ApiOperation("提交申请")
-    @GetMapping("insertRomLog")
+    @PostMapping("insertRomLog")
     public ResultDto<Integer> insertRomLog(@RequestBody RomsLog romsLog){
         return ResultDto.ok(romsService.insertRomLog(romsLog));
     }
